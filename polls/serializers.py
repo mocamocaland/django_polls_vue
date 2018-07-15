b1f521251b6507c5455b7af66a8398fa0bbfe315
mocamocaland <noi_kaas@yahoo.co.jp>
2018-07-15 22:45:16 +0900

    投票ボタンを追加中

--- polls/serializers.py
+++ polls/serializers.py
@@ -3,16 +3,6 @@ from rest_framework import serializers
 from .models import Question, Choice
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = (
-            'id',
-            'question_text',
-            'pub_date',
-        )
-
-
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
@@ -22,4 +12,17 @@ class ChoiceSerializer(serializers.ModelSerializer):
             'choice_text',
             'votes',
         )
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, source='choice_set')
+    class Meta:
+        model = Question
+        fields = (
+            'id',
+            'question_text',
+            'pub_date',
+            'choices',
+        )
+
+
 
