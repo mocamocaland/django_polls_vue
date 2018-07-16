fc0a8428afed9414ceff5b59b6ce864a24762914
mocamocaland <noi_kaas@yahoo.co.jp>
2018-07-16 17:55:14 +0900

    passwordを伏字に変更

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,11 +1,53 @@
 <template>
     <div>
-      loginページ
+      <v-alert :value="nonFieldErrors.length" type="error">
+        <div v-for="error in nonFieldErrors" :key="error">{{ error }}</div>
+      </v-alert>
+      <v-form v-model="valid">
+        <v-text-field
+          v-model="username"
+          label="ログインID"
+          required></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="パスワード"
+          :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+          :append-icon-cb=" () => (showPassword = !showPassword)"
+          :type="showPassword ? 'text' : 'password'"
+          :rules="passwordRules"
+          required></v-text-field>
+        <v-btn :disabled="!valid" @click="submit">ログイン</v-btn>
+      </v-form>
     </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
+  data () {
+    return {
+      valid: false,
+      username: null,
+      password: null,
+      showPassword: false,
+      nonFieldErrors: [],
+      usernameRules: [
+        v => !!v || 'ログインIDを入力してください',
+      ],
+      passwordRules: [
+        v => !!v || 'パスワードを入力してください',
+      ],
+    }
+  },
+  methods: {
+    submit () {
+      this.nonFieldErrors = []
+      this.$request.auth.login(this.username, this.password).then(res => {
+        console.log(res.data)
+      }, err => {
+        this.nonFieldErrors = err.response.data.nonFieldErrors
+      })
+    },
+  },
 }
 </script>
